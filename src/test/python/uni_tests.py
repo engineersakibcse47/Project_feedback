24d2cf7a6104ee82bf5daafcd246263a3a2fd608
unknown <shshimel288@gmail.com>
2022-12-08 01:03:28 +0600

    commit

--- src/test/python/uni_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import AttendanceProject
-
-class TestAttendanceProject(unittest.TestCase):
-    def test_add(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
