f76bcca8a385b3a83512663a0f07477c948aa5a9
Sakib Hosen Himel <108215990+engineersakibcse47@users.noreply.github.com>
2022-12-08 01:10:36 +0600

    Add files via upload

--- build.py
+++ build.py
@@ -4,9 +4,10 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.flake8")
-#use_plugin("python.coverage")
+use_plugin("python.coverage")
 use_plugin("python.distutils")
 use_plugin('python.pycharm')
+
 
 name = "Face Recognition"
 default_task = "publish"
@@ -14,4 +15,4 @@ default_task = "publish"
 
 @init
 def set_properties(project):
-    pass
+    project.set_property("coverage_break_build", False)
